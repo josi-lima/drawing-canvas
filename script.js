0a42fbbaa4a8dda533d66fe20a03814de6e44e44
Josiane de Lima <108018406+josi-lima@users.noreply.github.com>
2022-10-06 13:40:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Access canvas from the DOM 
 const canvas = document.querySelector('#draw');
 
-// Declare variables for getContext
+// Declare variables for .getContext()
 
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -9,4 +9,12 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#bada55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+
+// Variables for the mouse event as we draw
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+
 
