d2e4868feaa1a216aab343904fbfaff0130344a0
Josiane de Lima <108018406+josi-lima@users.noreply.github.com>
2022-10-06 14:12:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,5 +18,38 @@ let lastY = 0;
 let hue = 0;
 let direction = true;
 
+// Function for the mouse event
+
+function draw(e) {
+  if (!isDrawing) return; // stops the fn from running when moused down
+  console.log(e);
+
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+      
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY); // starts from
+  ctx.lineTo(e.offsetX, e.offsetY); // goes to
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // destructuring an array
+  
+  // Conditional to change color according to hsl
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+
+  // Conditional to change lineWidth
+
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction; // flip direction
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+}
+
+
 
 
