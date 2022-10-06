0d97c13ae36443c0793b3766670c046aa3f210a2
Josiane de Lima <108018406+josi-lima@users.noreply.github.com>
2022-10-06 13:40:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // Access canvas from the DOM 
 const canvas = document.querySelector('#draw');
+
+// Declare variables for getContext
+
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#bada55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
