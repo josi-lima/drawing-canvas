d3fbce1e59adf01dc0d506eb4c709fadfcb1b7e1
Josiane de Lima <108018406+josi-lima@users.noreply.github.com>
2022-10-07 11:24:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -70,26 +70,9 @@ canvas.addEventListener('touchstart', (e) => {
 });
 
 canvas.addEventListener('touchmove', draw);
-canvas.addEventListener('touchcancel', () => isDrawing = false);
 canvas.addEventListener('touchend', () => isDrawing = false);
 
-// Prevent scrolling when touching the canvas
 
-document.body.addEventListener("touchstart", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
-}, false);
-document.body.addEventListener("touchend", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
-}, false);
-document.body.addEventListener("touchmove", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
-}, false);
 
 
 
